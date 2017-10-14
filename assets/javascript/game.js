f6d672242d4db14e90f4266f642dcf81d0c89bef
Jason Saenz <jasonsaenz@Jasons-MacBook-Pro.local>
2017-10-14 18:57:26 -0500

    fixed random

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,15 +2,11 @@
 //define variables
 var wins = 0;
 var losses = 0;
-var randomNumber = Math.floor(Math.random() * 120) + 19;
+var randomNumber = Math.floor(Math.random() * 102) + 19;
 var buttonOne = Math.floor(Math.random() * 12) + 1;
 var buttonTwo = Math.floor(Math.random() * 12) + 1;
 var buttonThree = Math.floor(Math.random() * 12) + 1;
 var buttonFour = Math.floor(Math.random() * 12) + 1;
-
-//turn images into buttons
-
-//give each button a random value 1-12
 
 //define function that adds up the button presses
 
@@ -19,5 +15,18 @@ var buttonFour = Math.floor(Math.random() * 12) + 1;
 //define function that determines if the player loses, updates losses
 
 //define reset function
-
+function reset() {randomNumber;
+buttonOne;
+buttonTwo;
+buttonThree;
+buttonFour;
+}
 //input display of user's total
+function consoles(){console.log(randomNumber);
+console.log (buttonOne);
+console.log (buttonTwo);
+console.log (buttonThree);
+console.log (buttonFour);
+}
+
+consoles();
