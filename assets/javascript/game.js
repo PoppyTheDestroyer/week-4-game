f5253266c226f804a5a46731ec10a9aaf7e2ccbd
Jason Saenz <jasonsaenz@Jasons-MacBook-Pro.local>
2017-10-14 16:44:39 -0500

    some html

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+//pick random number from 19-120
+
+//input display of user's total
+
+//turn images into buttons
+
+//give each button a random value 1-12
+
+//define function that adds up the button presses
+
+//define function that determines if the player wins updates wins
+
+//define function that determines if the player loses, updates losses
+
+//define reset function
