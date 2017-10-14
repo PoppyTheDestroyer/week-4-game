7324c2e4ac69c2751827a05162c70a807e3eb9d9
Jason Saenz <jasonsaenz@Jasons-MacBook-Pro.local>
2017-10-14 17:04:43 -0500

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,9 @@
 //pick random number from 19-120
-
-//input display of user's total
-
+//define variables
+var wins = 0;
+var losses = 0;
+var randomNumber = Math.floor(Math.random() * 120) + 19;
+ 
 //turn images into buttons
 
 //give each button a random value 1-12
@@ -13,3 +15,5 @@
 //define function that determines if the player loses, updates losses
 
 //define reset function
+
+//input display of user's total
