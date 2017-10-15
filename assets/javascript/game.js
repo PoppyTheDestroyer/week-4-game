19c30b1e3bb132cf74bd072e539c4e2ef4406119
Jason Saenz <jasonsaenz@Jasons-MacBook-Pro.local>
2017-10-15 00:39:40 -0500

    it works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,36 +12,44 @@ $(document).ready(function () {
     var buttonThree = Math.floor(Math.random() * 12) + 1;
     var buttonFour = Math.floor(Math.random() * 12) + 1;
 
+
+    //define function that determines if the player wins/loses updates wins/losses
+    function winner() {
+        $("#result").text("You Win!");
+        wins ++;
+        $("#record").text("Wins: " + wins + " Losses: " + losses);        
+        reset();
+    };
+    function loser() {
+        if (userTotal > randomNumber) {
+            $("#result").text("You Lose!");
+            losses ++;
+            $("#record").text("Wins: " + wins + " Losses: " + losses);            
+            reset();
+        }
+
+    };
     //add jquery to display the target number
     $("#targetNumber").text("Number to guess: " + randomNumber);
 
     //add jquery to display user's guess
     $("#userNumber").text("Your total: " + userTotal);
 
-    //define function that determines if the player wins/loses updates wins/losses
-    function winner() {
-        $("#result").text("You Win!");
-        wins += 1;
-        reset();
-    };
-    function loser() {
-        if (userTotal > randomNumber) {
-            $("result").text("You Lose!");
-            losses += 1;
-            reset();
-        }
-
-    };
+    //display wins/losses
+    $("#record").text("Wins: " + wins + " Losses: " + losses);
 
     //define reset function
     function reset() {
-        userTotal = 0
         randomNumber = Math.floor(Math.random() * 102) + 19;
+        $("#targetNumber").text("Number to guess: " + randomNumber);
+        userTotal = 0
+        $("#userNumber").text("Your total: " + userTotal);
         buttonOne = Math.floor(Math.random() * 12) + 1;
         buttonTwo = Math.floor(Math.random() * 12) + 1;
         buttonThree = Math.floor(Math.random() * 12) + 1;
         buttonFour = Math.floor(Math.random() * 12) + 1;
-    }
+        consoles();
+    };
 
     //add jquery to assign value to the buttons
     //define function that adds up the button presses
