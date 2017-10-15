5c8b2bf0df0af28aece1ddd0692856ea9c2f0fa6
Jason Saenz <jasonsaenz@Jasons-MacBook-Pro.local>
2017-10-15 00:23:42 -0500

    so close

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,43 +13,82 @@ $(document).ready(function () {
     var buttonFour = Math.floor(Math.random() * 12) + 1;
 
     //add jquery to display the target number
-    $("#targetNumber").append("Number to guess: " + randomNumber);
+    $("#targetNumber").text("Number to guess: " + randomNumber);
 
     //add jquery to display user's guess
-    $("#userNumber").append("Your total: " + userTotal);
-    
-    //add jquery to assign value to the buttons
-    //define function that adds up the button presses
+    $("#userNumber").text("Your total: " + userTotal);
 
-    $("#crystalOne").on("click", function () {
-        userTotal = buttonOne + userTotal;
-    },
+    //define function that determines if the player wins/loses updates wins/losses
+    function winner() {
+        $("#result").text("You Win!");
+        wins += 1;
+        reset();
+    };
+    function loser() {
+        if (userTotal > randomNumber) {
+            $("result").text("You Lose!");
+            losses += 1;
+            reset();
+        }
 
-    $("#crystalTwo").on("click", function () {
-        userTotal = buttonTwo + userTotal;
-    },
-
-    $("#crystalThree").on("click", function () {
-        userTotal = buttonThree + userTotal;
-    },
-
-    $("#crystalFour").on("click", function () {
-        userTotal = buttonFour + userTotal;
-    }
-    
-
-    //define function that determines if the player wins updates wins
-
-    //define function that determines if the player loses, updates losses
+    };
 
     //define reset function
     function reset() {
-        randomNumber;
-        buttonOne;
-        buttonTwo;
-        buttonThree;
-        buttonFour;
+        userTotal = 0
+        randomNumber = Math.floor(Math.random() * 102) + 19;
+        buttonOne = Math.floor(Math.random() * 12) + 1;
+        buttonTwo = Math.floor(Math.random() * 12) + 1;
+        buttonThree = Math.floor(Math.random() * 12) + 1;
+        buttonFour = Math.floor(Math.random() * 12) + 1;
     }
+
+    //add jquery to assign value to the buttons
+    //define function that adds up the button presses
+    $("#crystalOne").on("click", function () {
+        userTotal = buttonOne + userTotal;
+        $("#userNumber").text("Your total: " + userTotal);
+        if (userTotal === randomNumber) {
+            winner()
+        }
+        else if (userTotal > randomNumber) {
+            loser();
+        }
+    }),
+
+        $("#crystalTwo").on("click", function () {
+            userTotal = buttonTwo + userTotal;
+            $("#userNumber").text("Your total: " + userTotal);
+            if (userTotal === randomNumber) {
+                winner()
+            }
+            else if (userTotal > randomNumber) {
+                loser();
+            }
+        }),
+
+        $("#crystalThree").on("click", function () {
+            userTotal = buttonThree + userTotal;
+            $("#userNumber").text("Your total: " + userTotal);
+            if (userTotal === randomNumber) {
+                winner()
+            }
+            else if (userTotal > randomNumber) {
+                loser();
+            }
+        }),
+
+        $("#crystalFour").on("click", function () {
+            userTotal = buttonFour + userTotal;
+            $("#userNumber").text("Your total: " + userTotal);
+            if (userTotal === randomNumber) {
+                winner()
+            }
+            else if (userTotal > randomNumber) {
+                loser();
+            }
+        })
+
     //input display of user's total
     function consoles() {
         console.log(randomNumber);
