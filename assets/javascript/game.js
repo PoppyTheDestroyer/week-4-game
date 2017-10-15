2a554d9a84d7dcec5e58047496eb5bf468fa85f6
Jason Saenz <jasonsaenz@Jasons-MacBook-Pro.local>
2017-10-14 23:37:10 -0500

    broken

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,50 @@
-//pick random number from 19-120
-//define variables
-var wins = 0;
-var losses = 0;
-var randomNumber = Math.floor(Math.random() * 102) + 19;
-var buttonOne = Math.floor(Math.random() * 12) + 1;
-var buttonTwo = Math.floor(Math.random() * 12) + 1;
-var buttonThree = Math.floor(Math.random() * 12) + 1;
-var buttonFour = Math.floor(Math.random() * 12) + 1;
+//load html first
+$(document).ready(function () {
 
-//define function that adds up the button presses
+    //pick random number from 19-120
+    //define variables
+    var wins = 0;
+    var losses = 0;
+    var userGuess = 0
+    var randomNumber = Math.floor(Math.random() * 102) + 19;
+    var buttonOne = Math.floor(Math.random() * 12) + 1;
+    var buttonTwo = Math.floor(Math.random() * 12) + 1;
+    var buttonThree = Math.floor(Math.random() * 12) + 1;
+    var buttonFour = Math.floor(Math.random() * 12) + 1;
 
-//define function that determines if the player wins updates wins
+    //add jquery to display the target number
+    $("#targetNumber").append("Number to guess: " + randomNumber);
 
-//define function that determines if the player loses, updates losses
+    //add jquery to display user's guess
+    $("#userNumber").append("Your number: " + userGuess);
+    
+    //add jquery to assign value to the buttons
+    $("#crystalOne").attr(buttonOne)
+    $("#crystalTwo").attr(buttonTwo)
+    $("#crystalThree").attr(buttonThree)
+    $("#crystalFour").attr(buttonFour)
+    
+    //define function that adds up the button presses
 
-//define reset function
-function reset() {randomNumber;
-buttonOne;
-buttonTwo;
-buttonThree;
-buttonFour;
-}
-//input display of user's total
-function consoles(){console.log(randomNumber);
-console.log (buttonOne);
-console.log (buttonTwo);
-console.log (buttonThree);
-console.log (buttonFour);
-}
+    //define function that determines if the player wins updates wins
 
-consoles();
+    //define function that determines if the player loses, updates losses
+
+    //define reset function
+    function reset() {
+        randomNumber;
+        buttonOne;
+        buttonTwo;
+        buttonThree;
+        buttonFour;
+    }
+    //input display of user's total
+    function consoles() {
+        console.log(randomNumber);
+        console.log(buttonOne);
+        console.log(buttonTwo);
+        console.log(buttonThree);
+        console.log(buttonFour);
+    }
+    consoles();
+});
